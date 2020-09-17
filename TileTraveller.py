fd4b5bdd27ae94f3fd7773d5149efb5086026f28
berglindb19 <berglindb19@ru.is>
2020-09-17 16:06:35 +0000

    Bull

--- TileTraveller.py
+++ TileTraveller.py
@@ -4,6 +4,6 @@
 # When the user enters the (3,1) tile he wins, it's the victory tile. 
 # At the beginning of the game the variable named victory = False
 # Also, at the beginning of the game we need to give beginning tile, which is (1,1)
-# Throughout the game a loop runs while victory != False 
+# Throughout the game a loop runss while victory != False 
 # Use if-sentences for every tile to tell the user in which dircetion he can travel
 # When the user/player wins the game, at tile (3,1) the victory variable turns into True
